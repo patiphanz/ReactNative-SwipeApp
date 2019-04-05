334d5225f7a22c0d1e99ca6a8d870859113e9ce1
patiphanz <patiphan.sr@ku.th>
2019-04-05 11:53:27 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screen/WelcomeScreen';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <WelcomeScreen/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screen/MenuScreen.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {Text,View} from 'react-native';
+
+class MenuScreen extends Component{
+  render(){
+    return (
+      <View>
+        <Text> This is Menu Screen</Text>
+      </View>
+    )
+  }
+}
+
+export default MenuScreen;
